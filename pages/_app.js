4026f836c4acd8bc57ee02d62adcec6ef3b5e35a
GuilhermeSsampaio <62760470+GuilhermeSsampaio@users.noreply.github.com>
2024-11-06 23:03:15 -0400

    correção deploy

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,6 @@
 import "@/styles/globals.css";
 import "bootstrap/dist/css/bootstrap.min.css";
-import "bootstrap/dist/js/bootstrap.bundle.min.js";
+// import "bootstrap/dist/js/bootstrap.bundle.min.js";
 export default function App({ Component, pageProps }) {
   return <Component {...pageProps} />;
 }
