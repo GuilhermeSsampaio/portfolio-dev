af93037a2e89cece45a55a989205f70119ff2f67
GuilhermeSsampaio <62760470+GuilhermeSsampaio@users.noreply.github.com>
2024-11-07 02:56:28 +0000

    card criado

--- components/Cards.jsx
+++ components/Cards.jsx
@@ -1,3 +1,34 @@
 export const Cards = () => {
-  return <div className="card">card de imagens de projetos</div>;
+  return (
+    <div className="col-md-6 col-lg-4 mb-4 d-flex justify-content-center">
+      <div className="card" style={{ maxWidth: "18rem", width: "100%" }}>
+        <svg
+          className="bd-placeholder-img card-img-top"
+          width="100%"
+          height="180"
+          xmlns="http://www.w3.org/2000/svg"
+          role="img"
+          aria-label="Placeholder: Image cap"
+          preserveAspectRatio="xMidYMid slice"
+          focusable="false"
+        >
+          <title>Placeholder</title>
+          <rect width="100%" height="100%" fill="#868e96"></rect>
+          <text x="50%" y="50%" fill="#dee2e6" dy=".3em">
+            Image cap
+          </text>
+        </svg>
+        <div className="card-body">
+          <h5 className="card-title">Card title</h5>
+          <p className="card-text">
+            Some quick example text to build on the card title and make up the
+            bulk of the card's content.
+          </p>
+          <a href="#" className="btn btn-primary">
+            Go somewhere
+          </a>
+        </div>
+      </div>
+    </div>
+  );
 };
