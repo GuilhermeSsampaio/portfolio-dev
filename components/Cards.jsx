5f9325e8c7a22dfdbdb45b973cfeb63e2bf4de05
GuilhermeSsampaio <62760470+GuilhermeSsampaio@users.noreply.github.com>
2024-11-06 23:00:16 -0400

    tirei apostrogo

--- components/Cards.jsx
+++ components/Cards.jsx
@@ -22,7 +22,7 @@ export const Cards = () => {
           <h5 className="card-title">Card title</h5>
           <p className="card-text">
             Some quick example text to build on the card title and make up the
-            bulk of the card's content.
+            bulk of the cards content.
           </p>
           <a href="#" className="btn btn-primary">
             Go somewhere
