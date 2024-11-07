1f096d6be4c5ee3f23788e84bcae12480848d22e
GuilhermeSsampaio <62760470+GuilhermeSsampaio@users.noreply.github.com>
2024-11-07 02:38:39 +0000

    title

--- components/Projects.jsx
+++ components/Projects.jsx
@@ -4,9 +4,9 @@ export const Projects = () => {
   return (
     <div className="projects">
       <h2 className="col">Projetos</h2>
-      <div className="">
-        <p className="col-6">Conheça meu git</p>
-        <SiGithub className="col-6" size={20} color="#181717" />
+      <div className="d-flex align-itens-center">
+        <p className="mb-0 me-2">Conheça meu git</p>
+        <SiGithub size={20} color="#181717" />
       </div>
       <p>Algumas criações próprias e a serviço da Embrapa</p>
       <Carousel />
