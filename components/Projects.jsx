2f5a65371b0fdb79aaa5d88f57faab96e44677e9
GuilhermeSsampaio <62760470+GuilhermeSsampaio@users.noreply.github.com>
2024-11-07 21:49:16 -0400

    melhorando css

--- components/Projects.jsx
+++ components/Projects.jsx
@@ -4,11 +4,6 @@ export const Projects = () => {
   return (
     <div className="projects content_section">
       <h2>Projetos</h2>
-      <div className="d-flex align-itens-center">
-        <p className="mb-0 me-2 fs-4">Conheça meu git</p>
-        <SiGithub size={30} color="#181717" />
-      </div>
-      <p className="fs-5">Algumas criações próprias e a serviço da Embrapa</p>
       <Cards />
     </div>
   );
