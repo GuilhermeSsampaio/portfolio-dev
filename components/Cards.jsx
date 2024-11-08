1ad0ea025d3fde23732e3b257aeaec7af9fb20fa
GuilhermeSsampaio <62760470+GuilhermeSsampaio@users.noreply.github.com>
2024-11-08 09:31:04 -0400

    otimizando card

--- components/Cards.jsx
+++ components/Cards.jsx
@@ -1,4 +1,4 @@
-export const Cards = () => {
+export const Cards = ({ imageCap, title, text }) => {
   return (
     <div className="col-md-6 col-lg-4 mb-4 d-flex justify-content-center">
       <div className="card" style={{ maxWidth: "18rem", width: "100%" }}>
@@ -15,17 +15,14 @@ export const Cards = () => {
           <title>Placeholder</title>
           <rect width="100%" height="100%" fill="#868e96"></rect>
           <text x="50%" y="50%" fill="#dee2e6" dy=".3em">
-            Image cap
+            {imageCap}
           </text>
         </svg>
         <div className="card-body">
-          <h5 className="card-title">Card title</h5>
-          <p className="card-text">
-            Some quick example text to build on the card title and make up the
-            bulk of the cards content.
-          </p>
+          <h5 className="card-title">{title}</h5>
+          <p className="card-text">{text}</p>
           <a href="#" className="btn btn-primary">
-            Go somewhere
+            Conhecer
           </a>
         </div>
       </div>
--- components/Projects.jsx
+++ components/Projects.jsx
@@ -4,7 +4,23 @@ export const Projects = () => {
   return (
     <div className="projects content_section">
       <h2>Projetos</h2>
-      <Cards />
+      <Cards
+        imageCap="Projeto 1"
+        title="Projeto 1"
+        text="Descrição do projeto 1"
+      />
+
+      <Cards
+        imageCap="Projeto 2"
+        title="Projeto 2"
+        text="Descrição do projeto 2"
+      />
+
+      <Cards
+        imageCap="Projeto 3"
+        title="Projeto 3"
+        text="Descrição do projeto 3"
+      />
     </div>
   );
 };
